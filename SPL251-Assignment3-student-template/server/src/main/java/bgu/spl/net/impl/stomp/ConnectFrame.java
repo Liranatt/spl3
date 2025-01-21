9baa1a4f036967990cbbb54e6c0df0309589f77a
nirhoro2 <nirhoro@post.bgu.ac.il>
2025-01-21 14:07:09 +0200

    added server constructor for connectFrame

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/ConnectFrame.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/ConnectFrame.java
@@ -1,11 +1,18 @@
 package bgu.spl.net.impl.stomp;
 
-public class connect extends Frame {
-    public connect(String username, String password){
+public class ConnectFrame extends Frame {
+
+    /// client constructor
+    public ConnectFrame(String username, String password) {
         super("CONNECT");
         this.addHeader("login", username);
         this.addHeader("passcode", password);
         this.addHeader("accept-version", "1.2");
         this.addHeader("host", "stomp.cs.bgu.ac.il");
+    }
+
+    /// server constructor
+    public ConnectFrame() {
+        super("CONNECT");
     }
 }
